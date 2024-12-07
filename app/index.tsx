d5e9cfe85cfa04e7045792dc641e279478421cb3
AbdallahIsmaili <aismaili691@gmail.com>
2024-12-07 20:20:18 +0000

    File & Folder Structure and Router Setup

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,7 @@
-import { Text, View } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { Redirect } from "expo-router";
 
-const Home = () => {
-  return (
-    <SafeAreaView>
-      <Text>RideOn!</Text>
-    </SafeAreaView>
-  );
+const Onboarding = () => {
+  return <Redirect href="/(auth)/welcome" />;
 };
 
-export default Home;
+export default Onboarding;
